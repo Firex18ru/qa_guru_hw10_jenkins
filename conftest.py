11b69d753dd060c093f8323b702d11b95cbf8835
Firex18ru <Firex1@yandex.ru>
2024-06-22 15:02:51 +0400

    do homework

--- conftest.py
+++ conftest.py
@@ -1,12 +1,26 @@
 import pytest
-from selene import Browser, Config
+from utils import attach
+from selene import browser
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from utils import attach
+
+from dotenv import load_dotenv
+
+import os
 
 
-@pytest.fixture(scope="function")
-def setup_browser(request):
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_settings():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com'
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -16,16 +30,23 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-    browser = Browser(Config(driver))
-    yield browser
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    yield
 
     attach.add_screenshot(browser)
-    attach.add_logs(browser)
     attach.add_html(browser)
+    attach.add_logs(browser)
     attach.add_video(browser)
+
     browser.quit()
